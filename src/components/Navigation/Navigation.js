be284018b7f0494b3df43f612cb0c8496b1b2eff
Serhii-Buha <serhii.buga@gmail.com>
2023-06-22 21:03:33 +0300

    add styles

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,15 +1,23 @@
 import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
 import { selectIsLoggedIn } from 'redux/auth/selectors';
+import { Link, Nav, NavList } from './Navigation.styled';
 
 export const Navigation = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return (
-    <nav>
-      <NavLink to="/">Home</NavLink>
+    <Nav>
+      <NavList>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
 
-      {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
-    </nav>
+        {isLoggedIn && (
+          <li>
+            <Link to="/contacts">Contacts</Link>
+          </li>
+        )}
+      </NavList>
+    </Nav>
   );
 };
