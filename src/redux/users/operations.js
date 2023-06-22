625cc589383dd74ddf96bc7d2133f0f1e856c8bf
Serhii-Buha <serhii.buga@gmail.com>
2023-06-22 20:56:42 +0300

    add react-hot-toast

--- src/redux/users/operations.js
+++ src/redux/users/operations.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { toast } from 'react-hot-toast';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
@@ -20,6 +21,7 @@ export const addContact = createAsyncThunk(
   async (contactData, thunkApi) => {
     try {
       const responce = await axios.post('/contacts', contactData);
+      toast.success(`${responce.data.name} is added to Phonebook.`);
       return responce.data;
     } catch (e) {
       return thunkApi.rejectWithValue(e.message);
@@ -32,6 +34,7 @@ export const deleteContact = createAsyncThunk(
   async (contactId, thunkApi) => {
     try {
       const responce = await axios.delete(`/contacts/${contactId}`);
+      toast.success(`${responce.data.name} is removed from the Phonebook.`);
       return responce.data;
     } catch (e) {
       return thunkApi.rejectWithValue(e.message);
@@ -44,6 +47,7 @@ export const updateContact = createAsyncThunk(
   async ({ id, name, number }, thunkAPI) => {
     try {
       const response = await axios.patch(`/contacts/${id}`, { name, number });
+      toast.success(`Contact ${name} is updated`);
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
