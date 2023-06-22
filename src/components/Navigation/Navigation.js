1be12b2dc13ccb84ea34b1f4b0a3b44ea960b4e1
Serhii-Buha <serhii.buga@gmail.com>
2023-06-22 11:23:40 +0300

    add Navigation, NavLink-Home, NavLink-Contacts

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { selectIsLoggedIn } from 'redux/auth/selectors';
+
+export const Navigation = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  return (
+    <nav>
+      <NavLink to="/">Home</NavLink>
+
+      {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
+    </nav>
+  );
+};
