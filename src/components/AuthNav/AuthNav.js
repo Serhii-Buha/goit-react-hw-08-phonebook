9e777c0097d8fe1c0fb32fd4699fafd269a309c9
Serhii-Buha <serhii.buga@gmail.com>
2023-06-22 11:21:46 +0300

    add AuthNav, NavLink-Register, NavLink-LogIn

---
+++ src/components/AuthNav/AuthNav.js
@@ -0,0 +1,9 @@
+import { NavLink } from 'react-router-dom';
+
+export const AuthNav = () => (
+  <div>
+    <NavLink to="/register">Register</NavLink>
+
+    <NavLink to="/login">Log In</NavLink>
+  </div>
+);
